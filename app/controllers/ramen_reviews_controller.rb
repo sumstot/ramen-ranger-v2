9f067e1c6ec6a8201c092bad218e2ebc38d4409c
Soren Umstot <soren.umstot@gmail.com>
2024-04-27 20:47:07 +0900

    Removed ransack query from index for start of development

--- app/controllers/ramen_reviews_controller.rb
+++ app/controllers/ramen_reviews_controller.rb
@@ -3,7 +3,7 @@ class RamenReviewsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @query = RamenReview.ransack(params[:q])
-    # @ramen_reviews = @ramen_reviews.all.includes(:ramen_review_photos)
+    # @query = RamenReview.ransack(params[:q])
+    @ramen_reviews = RamenReview.all.includes(:review_images)
   end
 end
