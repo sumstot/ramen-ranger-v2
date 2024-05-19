0000ad47b9900efd4d7df44d185081b6a2747e25
Soren Umstot <soren.umstot@gmail.com>
2024-05-19 16:31:26 +0900

    Added function for adding markers to google map

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -4,6 +4,10 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['map']
 
+  static values = {
+    markers: Array,
+  }
+
   connect() {
     window.addEventListener('initMap', this.initializeMap.bind(this))
     if (typeof google !== 'undefined' && typeof google.maps !== 'undefined') {
@@ -20,9 +24,19 @@ export default class extends Controller {
 
     const mapOptions = {
       center: { lat: 34.672314, lng: 135.484802 },
-      zoom: 10
+      zoom: 10,
     }
+    this.map = new google.maps.Map(this.mapTarget, mapOptions)
+    this.addMarkersToMap()
+  }
 
-    new google.maps.Map(this.mapTarget, mapOptions)
+  addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      console.log(marker)
+      new google.maps.Marker({
+        map: this.map,
+        position: { lat: parseFloat(marker.lat), lng: parseFloat(marker.lng) }
+      })
+    })
   }
 }
