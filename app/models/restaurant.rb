be1dfb27243be69990ba11a73113bed1b9d84672
Soren Umstot <soren.umstot@gmail.com>
2024-05-19 16:30:36 +0900

    Added geocoding condition to validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :ramen_reviews, dependent: :destroy
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :will_save_change_to_address?
 end
