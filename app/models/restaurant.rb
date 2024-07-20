c9cc6c32cc3b0ed6d8f8aae6baa446d067b2c277
Soren Umstot <soren.umstot@gmail.com>
2024-07-20 22:32:44 +0900

    inital commit for restaurants index

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -9,6 +9,17 @@ class Restaurant < ApplicationRecord
     update(average_score: ramen_reviews.average(:score).round(2))
   end
 
+  def display_images
+    first_images = ramen_reviews.order(id: :desc).flat_map { |ramen_review| ramen_review.review_images.first }.first(3)
+    binding.pry
+    if ramen_reviews.count < 3
+      remaining_images = ramen_reviews.order(id: :desc).flat_map(&:review_images).reject { |img| first_images.include?(img) }
+      remaining_images.take(3 - first_images.size)
+    else
+      first_images
+    end
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     %w[title content soup name jpn_name station city prefecture]
   end
