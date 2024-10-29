ad39311ca11b67047aafabff7981e4efffcc847b
Soren Umstot <soren.umstot@gmail.com>
2024-10-29 22:29:32 +0900

    Added acts_as_list gem to project and added acts_as_list to review_images

--- app/models/ramen_review.rb
+++ app/models/ramen_review.rb
@@ -3,7 +3,7 @@ class RamenReview < ApplicationRecord
   has_many :review_images, -> { order(position: :asc) }, dependent: :destroy
   has_rich_text :content
   after_save :update_restaurant_average_score
-  accepts_nested_attributes_for :review_images
+  accepts_nested_attributes_for :review_images, allow_destroy: true
 
   SOUP_TYPES = %w[Shoyu Shio Tonkotsu Niboshi Tantanmen Iekei Mazesoba Tsukemen Reimen Jiro].freeze
   POSSIBLE_SCORES = (0.0..5.0).step(0.1).map { |n| n.round(1) }.freeze
