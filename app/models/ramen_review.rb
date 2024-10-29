4d79e2005589a1acbc2a460d242d4a19ea926b74
Soren Umstot <soren.umstot@gmail.com>
2024-10-29 22:11:19 +0900

    Added position attribute to review_images and ordered review_images by position asc

--- app/models/ramen_review.rb
+++ app/models/ramen_review.rb
@@ -1,6 +1,6 @@
 class RamenReview < ApplicationRecord
   belongs_to :restaurant
-  has_many :review_images, dependent: :destroy
+  has_many :review_images, -> { order(position: :asc) }, dependent: :destroy
   has_rich_text :content
   after_save :update_restaurant_average_score
 
