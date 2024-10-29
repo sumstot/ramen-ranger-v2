200cd154c9ae37e704353cbd75c7e616dcef99a9
Soren Umstot <soren.umstot@gmail.com>
2024-10-29 21:33:18 +0900

    WIP for views

--- app/models/ramen_review.rb
+++ app/models/ramen_review.rb
@@ -5,6 +5,10 @@ class RamenReview < ApplicationRecord
   after_save :update_restaurant_average_score
   accepts_nested_attributes_for :review_images
 
+  SOUP_TYPES = %w[Shoyu Shio Tonkotsu Niboshi Tantanmen Iekei Mazesoba Tsukemen Reimen Jiro].freeze
+  POSSIBLE_SCORES = (0.0..5.0).step(0.1).map { |n| n.round(1) }.freeze
+  validates_numericality_of :score, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, on: create
+
   private
 
   def update_restaurant_average_score
