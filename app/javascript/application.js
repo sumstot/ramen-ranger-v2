75d030796c5a18ac243825a8ef49d5a62642c33c
Soren Umstot <soren.umstot@gmail.com>
2024-10-31 22:09:53 +0900

    Initial commit for adding dropzone to project.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,9 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
+import '@hotwired/turbo-rails'
 import './init_map'
-import "./controllers"
+import './helpers'
+import './controllers'
 // import 'swiper/swiper-bundle.css'
 
-import "trix"
-import "@rails/actiontext"
+import 'trix'
+import '@rails/actiontext'
