1b6f7c69379fb983bf93e7a92c622f7e90e5c2e4
Soren Umstot <soren.umstot@gmail.com>
2024-11-17 15:34:24 +0900

    WIP for adding turbo auto uploads to dropzone on ramen_reviews#new

--- app/controllers/ramen_reviews_controller.rb
+++ app/controllers/ramen_reviews_controller.rb
@@ -16,10 +16,25 @@ class RamenReviewsController < ApplicationController
     @ramen_review = RamenReview.new(ramen_review_params)
   end
 
+  def upload_image
+    @review_image = @review.review_images.build(image: params[:image])
+
+    if @review_image.save
+      render turbo_stream: [
+        turbo_stream.append('uploaded-images',
+                            partial: 'reviews/review_image_preview',
+                            locals: { review_image: @review_image }),
+        turbo_stream.update('upload-notice', 'Image uploaded successfully!')
+      ]
+    else
+      render turbo_stream: turbo_stream.update('upload-notice', 'Failed to upload image')
+    end
+  end
+
   private
 
   def ramen_review_params
-    params.require(:ramen_review).permit(:soup, :score, :review, :price, :restaurant_id, review_images: [:image])
+    params.require(:ramen_review).permit(:soup, :score, :review, :price, :restaurant_id, review_images_attributes: [:image])
   end
 
   def set_ramen_review
