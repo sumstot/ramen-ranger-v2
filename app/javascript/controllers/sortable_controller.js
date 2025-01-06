452964f7f41a9f56f04b976a338d13dd50fee2eb
Soren Umstot <soren.umstot@gmail.com>
2025-01-06 22:44:14 +0900

    Sortable functionality working

--- app/javascript/controllers/sortable_controller.js
+++ app/javascript/controllers/sortable_controller.js
@@ -1,12 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
+import Sortable from 'sortablejs'
 
 export default class extends Controller {
-  connect() {
-    console.log('sortable controller connected')
-  }
   static targets = ['previewImage']
 
-  previewImageTargetConnected() {
-    console.log(this.previewImageTargets)
+  connect() {
+    this.initSortable()
+  }
+
+  initSortable() {
+    this.sortable = new Sortable(this.element, {
+      animation: 150,
+      onEnd: (event) => this.updatePositions(event),
+    })
+  }
+
+  updatePositions() {
+    this.previewImageTargets.forEach((previewImage, index) => {
+      const positionInput = previewImage.querySelector(
+        'input[name*="[position]"]'
+      )
+      if (positionInput) {
+        positionInput.value = index
+      }
+    })
   }
 }
